78f801cd685656c1cb4f3e970366aa093ea69f82
Richard Glatter <rglatter.ieu2023@student.ie.edu>
2025-01-23 16:03:04 +0100

    Third Iteration of Code

--- HW2.py
+++ HW2.py
@@ -1,5 +1,8 @@
 try:
     GrossSalary = float(input("Enter your gross salary: "))
+    while GrossSalary < 0:
+        print("Please enter a valid number")
+        GrossSalary = float(input("Enter your gross salary: "))
     if GrossSalary < 1000:
         Income_Tax = 0.10
     elif 1000 <= GrossSalary < 2000:
@@ -10,16 +13,21 @@ try:
         Income_Tax = 0.18
 except ValueError:
     print("Please enter a valid number")
+
 try:
     Num_Children = int(input(f"Your Gross Income places you in a tax bracket with {Income_Tax} income tax, how many children do you have? "))
+    while Num_Children < 0:
+        print("Please enter a valid number")
+        Num_Children = int(input(f"Your Gross Income places you in a tax bracket with {Income_Tax} income tax, how many children do you have? "))
     if Income_Tax == 0.1 or Income_Tax == 0.12:
         Tax_Cut = 0.01
         Income_Tax_Child = Income_Tax - (0.01 * Num_Children)
+        print(Income_Tax_Child)
     else:
         Tax_Cut = 0.005
         Income_Tax_Child = Income_Tax - (0.005 * Num_Children)
-        print(Income_Tax_Child)
 except ValueError:
     print("Please enter a valid number")
+
 net_income = GrossSalary * (1-Income_Tax_Child)
 print(f"Your net income is: {net_income}")
