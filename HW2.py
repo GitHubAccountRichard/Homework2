c5c0c848e6287e59fb47dab3736df16799c38bcc
Richard Glatter <rglatter.ieu2023@student.ie.edu>
2025-01-23 16:24:27 +0100

    Final Iteration of Code Cleaned up some unnecessary repetitions

--- HW2.py
+++ HW2.py
@@ -1,8 +1,7 @@
 try:
     GrossSalary = float(input("Enter your gross salary: "))
     while GrossSalary < 0:
-        print("Please enter a valid number")
-        GrossSalary = float(input("Enter your gross salary: "))
+        GrossSalary = float(input("Please enter a valid salary: "))
     if GrossSalary < 1000:
         Income_Tax = 0.10
     elif 1000 <= GrossSalary < 2000:
@@ -16,13 +15,10 @@ except ValueError:
 try:
     Num_Children = int(input(f"Your Gross Income places you in a tax bracket with {Income_Tax} income tax, how many children do you have? "))
     while Num_Children < 0:
-        print("Please enter a valid number")
-        Num_Children = int(input(f"Your Gross Income places you in a tax bracket with {Income_Tax} income tax, how many children do you have? "))
+        Num_Children = int(input(f"Please enter a valid number of children "))
     if Income_Tax == 0.1 or Income_Tax == 0.12:
-        Tax_Cut = 0.01
         Income_Tax_Child = Income_Tax - (0.01 * Num_Children)
     else:
-        Tax_Cut = 0.005
         Income_Tax_Child = Income_Tax - (0.005 * Num_Children)
 except ValueError:
     print("Please enter a valid number")
